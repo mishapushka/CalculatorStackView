5198c0c9d08cac760efb0eaf9054c3be87185bae
Mikhail <bskfrost3@yandex.ru>
2021-04-15 17:36:07 +0700

    create cod start

--- CalculatorStackView/ViewController.swift
+++ CalculatorStackView/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var resultLabel: UILabel = {
+        let label = UILabel()
+
+        label.text = "0"
+        label.font = .systemFont(ofSize: 75, weight: .regular)
+        label.textColor = .white
+        label.textAlignment = .right
+        return label
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupHierarchy()
+        setupLayout()
+        setupView()
     }
 
+    // MARK: - Settings
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
+
+    private func setupView() {
+        view.backgroundColor = .black
+    }
+
+    //MARK: - Private functions
+
+    private func addCornerRadius(for buttons: [UIButton]) {
+        buttons.forEach {
+            $0.layer.masksToBounds = true
+            $0.layer.cornerRadius = Metric.buttonHeight / 2
+        }
+    }
 
 }
 
+// MARK: - Constants
+
+extension ViewController {
+
+    enum Metric {
+        static let buttonHeight: CGFloat = 75
+    }
+}
